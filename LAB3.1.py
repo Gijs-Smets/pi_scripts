07457f0e55b720631f722a59fa78c03cb01beeab
Gijs-Smets <r0925118@student.thomasmore.be>
2026-02-20 13:08:38 +0100

    intensity

--- LAB3.1.py
+++ LAB3.1.py
@@ -1,32 +1,25 @@
-import time
 import board
 import digitalio
+import time
+# Set pin number
+LED_PIN = board.D17
+# Initialize DigitalIO
+led = digitalio.DigitalInOut(LED_PIN)
+led.direction = digitalio.Direction.OUTPUT
+# Blinking function
+def blink(led_obj, number_blinks, period, duty_cycle):
 
-print("hello blinky!")
-
-board = {4 : digitalio.DigitalInOut(board.D4),
-         17 : digitalio.DigitalInOut(board.D17),
-         22 : digitalio.DigitalInOut(board.D22),
-         27 : digitalio.DigitalInOut(board.D27)
-         }
-
-for i in board:
-    board[i].direction = digitalio.Direction.OUTPUT
-
-def ledblink(boardnum,delay=0.5,blinknummer=1):
-    for i in range(blinknummer):
-        for j in boardnum:
-            led = board[j]
-            led.value = True
-        time.sleep(delay)
-        for j in boardnum:
-            led = board[j]
-            led.value = False      
-        time.sleep(delay)
-
-while True:
-    ledblink({4,17,22,27},0.2,3)
-    ledblink({4,17,22,27},0.7,3)
-    ledblink({4,17,22,27},0.2,3)
-    time.sleep(1)
+    # duty_cycle is a percentage (0-100)
+    time_high = period * (duty_cycle / 100)
+    time_low = period - time_high
+    for i in range(number_blinks):
+        led_obj.value = True
+        time.sleep(time_high)
+        led_obj.value = False
+        time.sleep(time_low)
+# Blink LED 20 times
+blink(led, 200, 0.01, 25)
+# Cleanup
+led.deinit() # Releases the pin
+print("Program executed")
 
