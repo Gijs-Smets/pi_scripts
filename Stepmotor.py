d3d9499090978c34a5fb39a33f0561bfb5e26380
Gijs-Smets <r0925118@student.thomasmore.be>
2026-02-27 13:42:51 +0100

    Stepmotor things

---
+++ Stepmotor.py
@@ -0,0 +1,59 @@
+import time
+import board
+import digitalio
+
+print("Turning")
+
+board = {4 : digitalio.DigitalInOut(board.D4),
+         17 : digitalio.DigitalInOut(board.D17),
+         27 : digitalio.DigitalInOut(board.D27),
+         22 : digitalio.DigitalInOut(board.D22)
+         }
+
+for i in board:
+    board[i].direction = digitalio.Direction.OUTPUT
+
+def step1():
+    board[4].value = True
+    board[27].value = False
+
+def step2():
+    board[17].value = True
+    board[22].value = False
+
+def step3():
+    board[27].value = True
+    board[4].value = False
+
+def step4():
+    board[22].value = True
+    board[17].value = False
+
+def wave(delay=0.002):
+    for i in board:
+        board[i].value = True
+        time.sleep(delay)
+        board[i].value = False
+
+def fullstep(delay=0.002):
+    step1()
+    time.sleep(delay)
+    step2()
+    time.sleep(delay)
+    step3()
+    time.sleep(delay)
+    step4()
+    time.sleep(delay)
+
+def fullstepR(delay=0.002):
+    step4()
+    time.sleep(delay)
+    step3()
+    time.sleep(delay)
+    step2()
+    time.sleep(delay)
+    step1()
+    time.sleep(delay)
+
+while True:
+    wave()
