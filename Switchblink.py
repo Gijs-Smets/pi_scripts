7ea6e93143ad998f53f72a64e1d2a57b27000f23
Gijs-Smets <r0925118@student.thomasmore.be>
2026-02-27 10:46:40 +0100

    Thigs and stuff

--- Switchblink.py
+++ Switchblink.py
@@ -7,11 +7,22 @@ pinIN = digitalio.DigitalInOut(board.D17)
 
 pinOUT.direction = digitalio.Direction.OUTPUT
 pinIN.direction = digitalio.Direction.INPUT
-pinIN.pull = digitalio.Pull.DOWN
 
+
+# pinIN.pull = digitalio.Pull.DOWN
+
+# while True:
+#     while not pinIN.value:   
+#         pinOUT.value = True
+#         time.sleep(0.3)
+#         pinOUT.value = False
+#         time.sleep(0.3)
+#     time.sleep(0.3)
+
+pinIN.pull = digitalio.Pull.UP
 
 while True:
-    while not pinIN:
+    while pinIN.value:   
         pinOUT.value = True
         time.sleep(0.3)
         pinOUT.value = False
